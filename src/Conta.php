1cd73838233988e9605f55299ef04e0724cbdcd5
unknown <lucas.hsilva@icloud.com>
2023-09-26 16:45:35 -0300

    alura

--- src/Conta.php
+++ src/Conta.php
@@ -4,7 +4,43 @@ class Conta
 {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
+
+    public function sacar(float $valorASacar)
+    {
+        if($valorASacar > $this->saldo){
+            echo "Valor indisponivel";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+        
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if($valorADepositar < 0)
+        {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+        
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if($valorATransferir > $this->saldo)
+        {
+            echo "Saldo indisponível";
+            return;
+        } 
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+        
+    }
 }
 
 ?>
