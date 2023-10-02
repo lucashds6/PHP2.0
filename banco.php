5d618808c61f354c502321f0c16ea795503ed1a4
unknown <lucas.hsilva@icloud.com>
2023-10-02 18:18:33 -0300

    alura

--- banco.php
+++ banco.php
@@ -1,19 +1,23 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
+require_once 'src/CPF.php';
 
-$primeiraConta = new Conta(cpfTitular: '123.456.789-10',nomeTitular: 'Vinicius Dias');
+$vinicius = new Titular(new CPF(numero:'123.456.789-10'), nome:'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(valorADepositar:500);
 $primeiraConta->saca(valorASacar:300);
 
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
-echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta(cpfTitular: '698.549.548-10', nomeTitular: 'Patricia');
+$patricia = new Titular (new CPF(numero:'698.549.548-10'), nome:'Patricia');
+$segundaConta = new conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(cpfTitular:'123', nomeTitular:'abcdefg');
+$outra = new Conta(new Titular (new CPF (numero:'123.634.345-23'), nome:'abcdefg'));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
 ?>
